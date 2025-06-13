9505f37e2fd49deff448f28538ca61157bc81c06
mbartlett21 <mjmouse9999@gmail.com>
2025-06-13 18:18:43 +1000

    Update driver script to be more reliable
    Anything run there could clear global variables.
    Also made it use pcall when calling tostring, as that could throw

--- driver.lua
+++ driver.lua
@@ -7,21 +7,42 @@ local device = require("device")
 
 local function nop() end
 
+-- driver.result: string | nil
 local driver = {name = "lunatik", open = nop, release = nop}
 
+-- cache functions so that we know that they are the original non-replaced ones
+local pcall, type, tostring, load, select
+	= pcall, type, tostring, load, select
+
+-- driver.read: function(Driver): string | nil
 function driver:read()
 	local result = self.result
 	self.result = nil
 	return result
 end
 
+-- result: function(_, ...: any): string
 local function result(_, ...)
-	return select("#", ...) > 0 and tostring(select(1, ...)) or ''
+	if select("#", ...) > 0 then
+		-- we need a pcall on tostring since it can call
+		-- metamethods __tostring or __name which can throw
+		local worked, str = pcall(tostring, select(1, ...))
+		if worked and type(str) == 'string' then
+			return str
+		else
+			return ''
+		end
+	else
+		return ''
+	end
 end
 
+-- driver.write: function(Driver, string)
 function driver:write(buf)
+	-- load never throws
 	local ok, err = load(buf)
 	if ok then
+		-- pcall since it is user code
 		err = result(pcall(ok))
 	end
 	self.result = err
